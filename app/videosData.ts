74a36dc72eb28a326866d90b30c3bdaf379f0fa9
Chetanbir Singh <chetanbirr@gmail.com>
2025-05-04 11:03:53 -0600

    Add typesafety for API return

--- app/videosData.ts
+++ app/videosData.ts
@@ -1,3 +1,16 @@
+export interface VideoInterface {
+  id: string;
+  title: string;
+  channel: string;
+  channelImage: string;
+  subscribers: string;
+  likes: number;
+  views: string;
+  timeAgo: string;
+  description: string;
+  thumbnail: string;
+}
+
 export async function getVideoData(videoId: string) {
   const res = await fetch(
     `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${videoId}&key=${process.env.YOUTUBE_API_KEY}`
@@ -14,7 +27,9 @@ export async function getVideoData(videoId: string) {
 
   const publishedAt = new Date(video.snippet.publishedAt);
   const now = new Date();
-  const diffInDays = Math.floor((now.getTime() - publishedAt.getTime()) / (1000 * 60 * 60 * 24));
+  const diffInDays = Math.floor(
+    (now.getTime() - publishedAt.getTime()) / (1000 * 60 * 60 * 24)
+  );
 
   let timeAgo = "Just now";
   if (diffInDays >= 365) {
@@ -26,11 +41,15 @@ export async function getVideoData(videoId: string) {
   } else if (diffInDays >= 1) {
     timeAgo = `${diffInDays} day(s) ago`;
   } else {
-    const diffInHours = Math.floor((now.getTime() - publishedAt.getTime()) / (1000 * 60 * 60));
+    const diffInHours = Math.floor(
+      (now.getTime() - publishedAt.getTime()) / (1000 * 60 * 60)
+    );
     if (diffInHours >= 1) {
       timeAgo = `${diffInHours} hour(s) ago`;
     } else {
-      const diffInMinutes = Math.floor((now.getTime() - publishedAt.getTime()) / (1000 * 60));
+      const diffInMinutes = Math.floor(
+        (now.getTime() - publishedAt.getTime()) / (1000 * 60)
+      );
       if (diffInMinutes >= 1) {
         timeAgo = `${diffInMinutes} minute(s) ago`;
       }
