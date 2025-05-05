b41e1bc5c1481f16ef1f5e6911fee7113d72bb86
Chetanbir Singh <chetanbirr@gmail.com>
2025-05-04 18:02:05 -0600

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["i.ytimg.com", "yt3.ggpht.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.ggpht.com",
+      },
+    ],
   },
-
 };
 
 export default nextConfig;
